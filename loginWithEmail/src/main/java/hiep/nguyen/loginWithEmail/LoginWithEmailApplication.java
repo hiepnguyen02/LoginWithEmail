ab9794fbe617f48d89559d2b7365c72d391c65ff
hiepnguyen02 <hiepnguyen.uet.vnu@gmail.com>
2023-09-19 14:11:15 +0700

    first commit

---
+++ loginWithEmail/src/main/java/hiep/nguyen/loginWithEmail/LoginWithEmailApplication.java
@@ -0,0 +1,13 @@
+package hiep.nguyen.loginWithEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithEmailApplication.class, args);
+	}
+
+}
---
+++ loginWithEmail/src/test/java/hiep/nguyen/loginWithEmail/LoginWithEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package hiep.nguyen.loginWithEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
